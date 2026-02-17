5a09f24fddae154ae5fc3fb2c7ff7964a34eb082
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-02-17 19:10:06 +0100

    Comentarios AuthManager

--- app/src/main/java/com/afundacion/fp/coruna/dashboards/server/AuthManager.java
+++ app/src/main/java/com/afundacion/fp/coruna/dashboards/server/AuthManager.java
@@ -3,36 +3,72 @@ package com.afundacion.fp.coruna.dashboards.server;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/*
+ * Esta clase se encarga de gestionar la sesión del usuario.
+ *
+ * Guarda y recupera el token de sesión utilizando SharedPreferences.
+ *
+ * Está implementada como Singleton, lo que significa
+ * que solo puede existir una instancia en toda la aplicación.
+ */
 public class AuthManager {
-    // Singleton
-    private static AuthManager instance = null;
-    private SharedPreferences preferences;
 
-    private static String TOKEN_KEY = "TOKEN";
+    private static AuthManager instance = null; // Instancia única (patrón Singleton)
+    private SharedPreferences preferences; // Objeto que permite guardar datos persistentes
+    private static String TOKEN_KEY = "TOKEN"; // Clave con la que se guarda el token
 
-    // private constructor
+    /*
+     * Constructor --> Solo se puede crear desde getInstance().
+     */
     private AuthManager(Context context) {
-        this.preferences = context.getSharedPreferences("DASHBOARDS_PREFS", Context.MODE_PRIVATE);
+
+        // Creamos/obtenemos un archivo de preferencias llamado DASHBOARDS_PREFS
+        this.preferences = context.getSharedPreferences(
+                "DASHBOARDS_PREFS",
+                Context.MODE_PRIVATE
+        );
     }
 
-    // public methods
+    /*
+     * METODO --> obtener la única instancia.
+     * Si no existe, se crea.
+     */
     public static AuthManager getInstance(Context context) {
+
         if (instance == null) {
             instance = new AuthManager(context);
         }
+
         return instance;
     }
 
+    /*
+     *  METODO --> Guarda el token de sesión en SharedPreferences.
+     * Se llama después de un login exitoso.
+     */
     public void saveSessionToken(String token) {
+
         SharedPreferences.Editor editor = preferences.edit();
+
+        // Guardamos el token con la clave TOKEN_KEY
         editor.putString(TOKEN_KEY, token);
+
+        // Confirmamos los cambios
         editor.commit();
     }
 
+    /*
+     * Recupera el token guardado -> Devuelve null si no existe
+     */
     public String getSessionToken() {
         return preferences.getString(TOKEN_KEY, null);
     }
 
+    /*
+     * Indica si el usuario está logueado.
+     * Si existe token → true
+     * Si no existe → false
+     */
     public boolean isLoggedIn() {
         return getSessionToken() != null;
     }
